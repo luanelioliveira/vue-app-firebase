a2d083c8b61276a33f8cb4bd5b876e3ba4b72ebe
Luan Eli Oliveira <luannn@gmail.com>
2019-06-21 22:51:52 -0300

    Adicionado componente toolbar

---
+++ src/components/Toolbar.vue
@@ -0,0 +1,26 @@
+<template>
+    <v-toolbar flat app color="primary" dark>
+      <v-toolbar-title class="headline text-uppercase">
+        <span>{{title}}</span>
+        <span class="font-weight-light">{{subtitle}}</span>
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn flat to="/" >
+        <span>Home</span>
+      </v-btn>
+      <v-btn flat to="/about" >
+        <span>About</span>
+      </v-btn>
+    </v-toolbar>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      title: 'Example',
+      subtitle: 'App',
+    };
+  },
+};
+</script>
