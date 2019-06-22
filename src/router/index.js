ae1649097215eb80f7d87b1f8beae97941c5edd3
Luan Eli Oliveira <luannn@gmail.com>
2019-06-22 16:54:34 -0300

    Ajuste do ESLint

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
+// eslint-disable-next-line
+import store from '@/store';
 import Vue from 'vue';
 import Router from 'vue-router';
-import store from '@/store';
 import routes from './routerConfig';
 
 Vue.use(Router);
--- src/router/routerConfig.js
+++ src/router/routerConfig.js
@@ -34,5 +34,5 @@ export default [
   {
     path: '*',
     component: NotFound,
-  }
+  },
 ];
