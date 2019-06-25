f81500488aa7c92c429201aa064f09eef1605557
Luan Eli Oliveira <luannn@gmail.com>
2019-06-24 22:34:57 -0300

    Adicionada pagina do meu perfil

---
+++ src/views/ProfileView.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <title-page :text="titlePage" />
+    <v-flex>
+    </v-flex>
+  </div>
+</template>
+
+<script>
+
+export default {
+  data() {
+    return {
+      titlePage: 'Meu Perfil',
+    };
+  },
+};
+</script>
