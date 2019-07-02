80161f3f87d98e22b2a956cb778827ae00c3db8d
Luan Eli Oliveira <luannn@gmail.com>
2019-07-02 19:58:55 -0300

    Adicionado novo componente de loader

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,7 @@
     <TheNavigationDrawer />
     <TheToolbar />
     <v-content transition="fade-transition">
+        <Loading />
         <router-view/>
     </v-content>
   </v-app>
@@ -11,12 +12,14 @@
 <script>
 import TheToolbar from './components/TheToolbar.vue';
 import TheNavigationDrawer from './components/TheNavigationDrawer.vue';
+import Loading from '@/components/LoadingPage.vue';
 
 export default {
   name: 'App',
   components: {
     TheToolbar,
     TheNavigationDrawer,
+    Loading,
   },
   data() {
     return {
--- src/components/LoadingPage.vue
+++ src/components/LoadingPage.vue
@@ -1,10 +1,26 @@
 <template>
-  <v-layout>
-    <v-flex>
-      <v-progress-circular
-        indeterminate
-        color="primary"
-      ></v-progress-circular>
-    </v-flex>
-  </v-layout>
+  <loading
+  :active.sync="loading"
+  :can-cancel="false"
+  :is-full-page="false"
+  :width="45"
+  :height="45"
+  :opacity="0.8"
+  :color="'#4A148C'">
+  </loading>
 </template>
+
+<script>
+import { mapState } from 'vuex';
+import Loading from 'vue-loading-overlay';
+import 'vue-loading-overlay/dist/vue-loading.css';
+
+export default {
+  computed: {
+    ...mapState('Application', ['loading']),
+  },
+  components: {
+    Loading,
+  },
+};
+</script>
